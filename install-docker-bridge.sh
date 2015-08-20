d175be0d0dfaead29baaff7799be49db5a8a7bb7
bharbhi <bhargav.ietf@gmail.com>
2015-08-20 14:15:43 -0700

    Create install-docker-bridge.sh

---
+++ install-docker-bridge.sh
@@ -0,0 +1,7 @@
+wget -qO- https://get.docker.io/gpg | sudo apt-key add -
+sudo sh -c "echo deb http://get.docker.io/ubuntu docker main > /etc/apt/sources.list.d/docker.list"
+sudo apt-get update
+sudo apt-get install -y lxc-docker
+
+#install bridge utils
+sudo apt-get install -y bridge-utils
