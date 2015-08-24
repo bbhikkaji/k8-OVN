c96b4977c84a8510cd78123a2057b1ad90e4c54f
bharbhi <bhargav.ietf@gmail.com>
2015-08-24 16:07:58 -0700

    Update k8-install-configure.sh

--- k8-install-configure.sh
+++ k8-install-configure.sh
@@ -1,4 +1,12 @@
+#install git
+sudo apt-get install -y git
+
+#install Kubernetes
 git clone https://github.com/GoogleCloudPlatform/kubernetes.git
+
+#install OVN
 cd kubernetes/cluster/ubuntu
-git clone https://github.com/shettyg/ovn-docker.git
-cd ..
+git clone https://github.com/bharbhi/k8-OVN.git
+
+#Update build.sh
+cp k8-OVN/build.sh .
