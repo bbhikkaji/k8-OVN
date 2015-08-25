a25afbee08715308b8b4b1e50978ed669836bb0d
bharbhi <bhargav.ietf@gmail.com>
2015-08-25 07:33:04 -0700

    Update setup-ssh.sh

--- setup-ssh.sh
+++ setup-ssh.sh
@@ -2,7 +2,13 @@
 sudo apt-get install sshpass
 
 #Generate Key & Copy it to Minions.
+su - vagrant
+echo "getting on to vagrant "
 ssh-keygen -b 2048 -t rsa -f ~/.ssh/id_rsa -q -N ""
-sshpass -p vagrant ssh-copy-id 10.10.103.250
-sshpass -p vagrant ssh-copy-id 10.10.103.223
-sshpass -p vagrant ssh-copy-id 10.10.103.162
+echo $?
+sshpass -p vagrant ssh-copy-id -o StrictHostKeyChecking=no vagrant@10.10.103.250
+echo $?
+sshpass -p vagrant ssh-copy-id -o StrictHostKeyChecking=no vagrant@10.10.103.223
+echo $?
+sshpass -p vagrant ssh-copy-id -o StrictHostKeyChecking=no vagrant@10.10.103.162
+echo $?
